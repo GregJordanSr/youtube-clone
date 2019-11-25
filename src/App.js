472546c8aeb3070438e5befd43d4bcb4fb6ea806
GregJordanSr <gregljordan@gmail.com>
2019-11-24 22:40:49 -0500

    Mapped over videos, styled and adding logic to VideoItem component

--- src/App.js
+++ src/App.js
@@ -5,44 +5,54 @@ import { SearchBar, VideoList, VideoDetail } from "./components";
 import youtube from './api/youtube';
 
 class App extends React.Component {
-    state = {
-        video: [],
-        selectedVideo: null,
-    }
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
 
-    handleSubmit = async (searchTerm) => {
-        const response = await youtube.get("search",{
-            params: {
-                part: 'snippet',
-                maxResults: 10,
-                key: process.env.REACT_APP_API_KEY,
-                q: searchTerm,
-          }
-        });
+  handleSubmit = async searchTerm => {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 10,
+        key: process.env.REACT_APP_API_KEY,
+        q: searchTerm
+      }
+    });
 
-        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] }); 
-    }
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
 
-    render (){
-        const { selectedVideo} = this.state;
-        return(
-            <Grid justify="center" container spacing={10}>
-                <Grid item xs={12}>
-                    <Grid container spacing={10}>
-                        <Grid item xs={12}>
-                           <SearchBar  onFormSubmit={this.handleSubmit} />
-                        </Grid>
-                        <Grid item xs={8}>
-                            <VideoDetail video={selectedVideo} />
-                        </Grid>
-                        <Grid item xs={4}>
-                           <VideoList />
-                        </Grid>
-                    </Grid>
-                </Grid>
+  onSelectVideo = () => {
+
+  }
+
+  render() {
+    const { selectedVideo, videos } = this.state;
+    return (
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
             </Grid>
-        )
-    }
+            <Grid item xs={8}>
+              <VideoDetail
+                video={selectedVideo}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <VideoList videos={videos} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import { Grid } from '@material-ui/core';
+import VideoItem  from "./VideoItem";
 
-const VideoList = () => {
-    return (
-        <Grid>With that said</Grid>
-    )
-}
+const VideoList = ({ videos, onVideoSelect }) => {
+  const listVideos = videos.map((video, id) => (
+    <VideoItem onVideoSelect={onVideoSelect} key={id} video={video} />
+  ));
+
+  return (
+    <Grid container spacing={10}>
+      {listVideos}
+    </Grid>
+  );
+};
 
 export default VideoList;
